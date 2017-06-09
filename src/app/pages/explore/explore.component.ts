ca1a695153b64014c3188acef18a19f10bfffbc0
ffalt <ffalty@gmail.com>
2017-06-09 16:18:05 +0200

    generic indicator graphs for administrative quality

--- src/app/pages/explore/explore.component.ts
+++ src/app/pages/explore/explore.component.ts
@@ -12,9 +12,9 @@ export class ExplorePage {
 	public cards = [
 		{icon: 'icon-hammer', url: '/explore/sectors', title: 'Sectors', subtitle: 'Explore Structure of Tenders', text: Consts.IPSUM},
 		{icon: 'icon-flag', url: '/explore/corruption-indicators', title: 'Corruption Risk', subtitle: 'Explore Corruption Risk Indicators', text: Consts.IPSUM},
+		{icon: 'icon-library', url: '/explore/administrative-quality', title: 'Administrative Quality', subtitle: 'Explore Administrative Quality', text: Consts.IPSUM},
 		{icon: 'icon-checkmark', url: '/explore/transparency-indicators', title: 'Transparency', subtitle: 'Explore Transparency Indicators', text: Consts.IPSUM},
 		{icon: 'icon-briefcase', url: '/explore/rankings', title: 'Rankings', subtitle: 'Explore Top/Bottom Performers', text: Consts.IPSUM},
-		{icon: 'icon-library', url: '/explore/administrative-quality', title: 'Quality', subtitle: 'Explore Administrative Quality', text: Consts.IPSUM},
 		{icon: 'icon-history', url: '/explore/latest', title: 'Latest', subtitle: 'Explore Latest Tenders', text: Consts.IPSUM}
 	];
 
