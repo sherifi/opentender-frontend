9b073e25d4771c8f36b8b54ac903bc33200c536f
ffalt <ffalty@gmail.com>
2017-07-25 16:05:10 +0200

    cleared test page

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {PlatformService} from '../../services/platform.service';
 import {ApiService} from '../../services/api.service';
-import {IStatsNuts} from '../../app.interfaces';
 
 @Component({
 	moduleId: __filename,
@@ -9,49 +8,8 @@ import {IStatsNuts} from '../../app.interfaces';
 	templateUrl: 'test.template.html'
 })
 export class TestPage {
-	private map_level: number = 1;
-	private map_companies: boolean = false;
-	private map_data: IStatsNuts = null;
 
 	constructor(private api: ApiService, private platform: PlatformService) {
-		this.fillMap();
 	}
 
-	toggle() {
-		this.map_companies = !this.map_companies;
-		this.fillMap();
-	}
-
-	fillMap() {
-		if (this.map_companies) {
-			this.api.getCompanyNutsStats().subscribe(
-				res => {
-					this.map_data = res.data;
-				},
-				err => {
-					console.error(err);
-				},
-				() => {
-					// console.log('nuts complete');
-				}
-			);
-		} else {
-			this.api.getAuthorityNutsStats().subscribe(
-				res => {
-					this.map_data = res.data;
-				},
-				err => {
-					console.error(err);
-				},
-				() => {
-					// console.log('nuts complete');
-				}
-			);
-		}
-	}
-
-	setLevel(level) {
-		this.map_level = level;
-		this.fillMap();
-	}
 }
