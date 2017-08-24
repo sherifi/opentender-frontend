9a8967f87bcaec778924cc6237b730185e2cbeca
ffalt <ffalty@gmail.com>
2017-08-24 17:58:40 +0200

    show more on tender profil page

--- src/app/pages/tender/tender.component.ts
+++ src/app/pages/tender/tender.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute} from '@angular/router';
 import {ApiService} from '../../services/api.service';
 import {PlatformService} from '../../services/platform.service';
 import {ConfigService, Country} from '../../services/config.service';
+import {Consts} from '../../model/consts';
 
 @Component({
 	moduleId: __filename,
@@ -17,13 +18,20 @@ export class TenderPage implements OnInit, OnDestroy {
 	public state = {
 		lots: {$open: true},
 		buyer: {$open: true},
+		indi: {$open: true},
 		info: {$open: true},
 		desc: {$open: true},
+		reqs: {$open: false},
 		additional: {$open: false},
 		documents: {$open: false},
 		publications: {$open: false},
 		raw: {$open: false},
 		discussion: {$open: false},
+	};
+	public indicators = {
+		cr: [],
+		tr: [],
+		ac: []
 	};
 
 	constructor(private route: ActivatedRoute, private api: ApiService, private config: ConfigService, private platform: PlatformService) {
@@ -31,6 +39,7 @@ export class TenderPage implements OnInit, OnDestroy {
 			this.state.additional.$open = true;
 			this.state.documents.$open = true;
 			this.state.publications.$open = true;
+			this.state.reqs.$open = true;
 			this.state.raw.$open = true;
 			this.state.discussion.$open = true;
 		}
@@ -55,6 +64,22 @@ export class TenderPage implements OnInit, OnDestroy {
 
 	display(tender: Definitions.Tender): void {
 		this.tender = tender;
+
+		this.indicators.ac = [];
+		this.indicators.cr = [];
+		this.indicators.tr = [];
+		if (tender.indicators) {
+			tender.indicators.forEach(indicator => {
+				if (indicator.type.indexOf(Consts.indicators.ac.prefix) === 0) {
+					this.indicators.ac.push(indicator);
+				} else if (indicator.type.indexOf(Consts.indicators.cr.prefix) === 0) {
+					this.indicators.cr.push(indicator);
+				} else if (indicator.type.indexOf(Consts.indicators.tr.prefix) === 0) {
+					this.indicators.tr.push(indicator);
+				}
+			});
+		}
+
 		this.tender_raw = JSON.stringify(tender, null, '\t');
 	}
 
