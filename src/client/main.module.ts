6b5013b93fe2aab4a6933c5764d7e42b5f7b1f7c
ffalt <ffalty@gmail.com>
2017-10-25 11:04:23 +0200

    test piwik angular integration

--- src/client/main.module.ts
+++ src/client/main.module.ts
@@ -5,6 +5,7 @@ import {HttpModule} from '@angular/http';
 import {LeafletModule} from '@asymmetrik/ngx-leaflet';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {BrowserPrebootModule} from 'preboot/browser';
+import {Angulartics2Module, Angulartics2Piwik} from 'angulartics2';
 
 @NgModule({
 	bootstrap: [App],
@@ -17,14 +18,16 @@ import {BrowserPrebootModule} from 'preboot/browser';
 		BrowserPrebootModule.replayEvents(),
 		BrowserAnimationsModule,
 		LeafletModule.forRoot(),
-		...AppConfig.imports
+		...AppConfig.imports,
+		Angulartics2Module.forRoot([Angulartics2Piwik]) // include after routes
 	],
 	providers: [
 		...AppConfig.providers,
 		{provide: 'absurl', useValue: null},
 		{provide: 'opentender', useValue: null},
 		{provide: 'isBrowser', useValue: true},
-		{provide: 'globals', useValue: {window: window, document: document}}
+		{provide: 'globals', useValue: {window: window, document: document}},
+		Angulartics2Piwik
 	]
 })
 export class MainModule {
