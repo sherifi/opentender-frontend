14ef01de49b847c0d924428abd8287cb940377c4
ffalt <ffalty@gmail.com>
2017-11-14 09:03:05 +0100

    changed order of dashboards

--- src/app/pages/dashboards/dashboards.component.ts
+++ src/app/pages/dashboards/dashboards.component.ts
@@ -11,9 +11,9 @@ export class DashboardsPage implements OnInit {
 
 	constructor(i18n: I18NService) {
 		this.cards = [
-			{icon: 'icon-flag', url: '/dashboards/procurement-integrity', title: i18n.get('Procurement Integrity')},
 			{icon: 'icon-library', url: '/dashboards/administrative-capacity', title: i18n.get('Administrative Capacity')},
 			{icon: 'icon-checkmark', url: '/dashboards/transparency', title: i18n.get('Transparency')},
+			{icon: 'icon-flag', url: '/dashboards/procurement-integrity', title: i18n.get('Procurement Integrity')},
 			{icon: 'icon-hammer', url: '/dashboards/market-analysis', title: i18n.get('Market Analysis')}
 		];
 	}
