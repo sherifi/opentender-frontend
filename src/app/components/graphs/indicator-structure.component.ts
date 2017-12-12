f61c91c548a176eaee7bebd2dc295298d472d1a5
ffalt <ffalty@gmail.com>
2017-12-12 13:03:00 +0100

    removed invalid language extract

--- src/app/components/graphs/indicator-structure.component.ts
+++ src/app/components/graphs/indicator-structure.component.ts
@@ -7,7 +7,7 @@ import {Consts} from '../../model/consts';
 @Component({
 	selector: 'graph[indicator-structure]',
 	template: `
-		<div class="graph-title" i18n>{{title}}</div>
+		<div class="graph-title">{{title}}</div>
 		<ngx-charts-radar-chart
 				class="chart-container"
 				[chart]="graph.chart"
