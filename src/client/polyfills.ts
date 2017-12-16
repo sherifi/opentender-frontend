c5f1a25411a295ca546a00299ab8647c78f84933
ffalt <ffalty@gmail.com>
2017-12-16 22:18:23 +0100

    polyfills for safari

--- src/client/polyfills.ts
+++ src/client/polyfills.ts
@@ -1,2 +1,8 @@
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
+/** Evergreen browsers require these. **/
+import 'core-js/es6/object';
+import 'core-js/es6/string';
+import 'core-js/es6/array';
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
