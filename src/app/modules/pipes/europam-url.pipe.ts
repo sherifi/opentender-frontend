3387c4ea098ef9c960c3402c5aeefc0010ff1ac4
ffalt <ffalty@gmail.com>
2018-01-11 14:41:42 +0100

    europam links from eu data

--- src/app/modules/pipes/europam-url.pipe.ts
+++ src/app/modules/pipes/europam-url.pipe.ts
@@ -1,10 +1,20 @@
 import {Pipe, PipeTransform} from '@angular/core';
+import {Consts} from '../../model/consts';
 
 @Pipe({name: 'europamLink'})
 export class EuropamLinkPipe implements PipeTransform {
 
 	transform(value: string): string {
-		return 'http://europam.eu/?module=country-profile' + (value !== 'Europe' ? '&country=' + value : '') + '#info_PP';
+		if (!value) {
+			return 'http://europam.eu/';
+		}
+		let country = Consts.countries[value.toUpperCase()];
+		if (!country) {
+			if (value === 'eu') {
+				country = 'European Commission';
+			}
+		}
+		return 'http://europam.eu/?module=country-profile' + (country !== null ? '&country=' + country : '') + '#info_PP';
 	}
 
 }
