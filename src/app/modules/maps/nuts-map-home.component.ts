dbfacb54f90ed96eb7028331d54dacd5a82de4cf
ffalt <ffalty@gmail.com>
2018-01-19 14:21:52 +0100

    remove outlier authority NUTS from home map

--- src/app/modules/maps/nuts-map-home.component.ts
+++ src/app/modules/maps/nuts-map-home.component.ts
@@ -3,27 +3,29 @@ import {ApiService} from '../../services/api.service';
 import {IStatsNuts} from '../../app.interfaces';
 import {NotifyService} from '../../services/notify.service';
 import {I18NService} from '../i18n/services/i18n.service';
+import {ConfigService} from '../../services/config.service';
 
 @Component({
 	moduleId: __filename,
 	selector: 'home-nutsmap',
-	template: `<div class="graph-header">
-	<div class="graph-title">{{title}}</div>
-	<div class="graph-toolbar-container">
-		<div class="graph-toolbar graph-toolbar-left">
-			<button class="tool-button" [ngClass]="{down:!map_companies}" (click)="toggle()" i18n>Buyers</button>
-			<button class="tool-button" [ngClass]="{down:map_companies}" (click)="toggle()" i18n>Suppliers</button>
+	template: `
+		<div class="graph-header">
+			<div class="graph-title">{{title}}</div>
+			<div class="graph-toolbar-container">
+				<div class="graph-toolbar graph-toolbar-left">
+					<button class="tool-button" [ngClass]="{down:!map_companies}" (click)="toggle()" i18n>Buyers</button>
+					<button class="tool-button" [ngClass]="{down:map_companies}" (click)="toggle()" i18n>Suppliers</button>
+				</div>
+				<div class="graph-toolbar graph-toolbar-right">
+					<button class="tool-button" [ngClass]="{down:map_level==0}" (click)="setLevel(0)" i18n>Countries</button>
+					<button class="tool-button" [ngClass]="{down:map_level==1}" (click)="setLevel(1)" i18n>NUTS 1</button>
+					<button class="tool-button" [ngClass]="{down:map_level==2}" (click)="setLevel(2)" i18n>NUTS 2</button>
+					<button class="tool-button" [ngClass]="{down:map_level==3}" (click)="setLevel(3)" i18n>NUTS 3</button>
+				</div>
+			</div>
 		</div>
-		<div class="graph-toolbar graph-toolbar-right">
-			<button class="tool-button" [ngClass]="{down:map_level==0}" (click)="setLevel(0)" i18n>Countries</button>
-			<button class="tool-button" [ngClass]="{down:map_level==1}" (click)="setLevel(1)" i18n>NUTS 1</button>
-			<button class="tool-button" [ngClass]="{down:map_level==2}" (click)="setLevel(2)" i18n>NUTS 2</button>
-			<button class="tool-button" [ngClass]="{down:map_level==3}" (click)="setLevel(3)" i18n>NUTS 3</button>
-		</div>
-	</div>
-</div>
-<graph nutsmap [data]="map_data" [level]="map_level" [formatTooltip]="formatTooltip" [title]="title"></graph>
-`
+		<graph nutsmap [data]="map_data" [level]="map_level" [formatTooltip]="formatTooltip" [title]="title"></graph>
+	`
 })
 export class MapHomeComponent {
 	public map_level: number = 2;
@@ -33,7 +35,7 @@ export class MapHomeComponent {
 	public loading: number = 0;
 	public title: string = '';
 
-	constructor(private api: ApiService, private notify: NotifyService, private i18n: I18NService) {
+	constructor(private api: ApiService, private notify: NotifyService, private i18n: I18NService, private config: ConfigService) {
 		this.formatTooltip = this.formatTooltipCallback.bind(this);
 		this.fillMap(this.map_level);
 	}
@@ -73,7 +75,17 @@ export class MapHomeComponent {
 			let sub = this.api.getAuthorityNutsStats().subscribe(
 				(result) => {
 					this.map_level = level;
-					this.map_data = result.data;
+					if (['eu', 'all'].indexOf(this.config.country.id) < 0) {
+						let filtered = {};
+						Object.keys(result.data).forEach(key => {
+							if (key.indexOf(this.config.country.id) === 0) {
+								filtered[key] = result.data[key];
+							}
+						});
+						this.map_data = filtered;
+					} else {
+						this.map_data = result.data;
+					}
 				},
 				(error) => {
 					this.notify.error(error);
