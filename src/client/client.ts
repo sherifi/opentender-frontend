0400ab955a08562734894c2a432aa7345438bed1
ffalt <ffalty@gmail.com>
2018-01-23 12:59:54 +0100

    disable missing translation warning in production mode

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,14 +3,14 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {MainModule} from './client.module';
 import {getTranslationProviders} from './i18n-providers';
 import {environment} from '../environments/environment';
+import {MissingTranslationStrategy} from '@angular/core';
 
 const init = () => {
 	getTranslationProviders().then(providers => {
-		const options = {providers};
-		// here we pass "options.providers" to "platformBrowserDynamic" as extra providers.
-		// otherwise when we inject the token TRANSLATIONS it will be empty. The second argument of
-		// "bootstrapModule" will assign the providers to the compiler and not our AppModule
-		platformBrowserDynamic(<StaticProvider[]>options.providers).bootstrapModule(MainModule, options);
+		platformBrowserDynamic(<StaticProvider[]>providers).bootstrapModule(MainModule, {
+			missingTranslation: environment.production ? MissingTranslationStrategy.Ignore : MissingTranslationStrategy.Warning,
+			providers
+		});
 	});
 };
 if (environment.production) {
