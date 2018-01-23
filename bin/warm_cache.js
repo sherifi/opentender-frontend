f996629a74cd7a402d40923eb011d607e4a73556
ffalt <ffalty@gmail.com>
2018-01-23 11:53:42 +0100

    warm cache for all languages

--- bin/warm_cache.js
+++ bin/warm_cache.js
@@ -17,30 +17,42 @@ if (config.server.data.path[0] === '.') {
 	data_folder = path.resolve(__dirname, '..', config.server.data.path);
 }
 let portals = JSON.parse(fs.readFileSync(path.join(data_folder, 'portals.json')).toString());
-let sites = ['/',
-	'/search/tender',
-	'/search/company',
-	'/search/authority',
-	'/dashboards/market-analysis',
-	'/dashboards/integrity',
-	'/dashboards/transparency',
-	'/dashboards/administrative-capacity'
-];
+let routes = JSON.parse(fs.readFileSync(path.join('../src/app/model', 'routes.json')).toString()).routes;
+let languages = JSON.parse(fs.readFileSync(path.join('../src/i18n', 'languages.json')).toString()).enabled;
 
-let warm_site = (site, cb) => {
-	async.forEachSeries(portals, (portal, next) => {
-		let url = 'http://' + config.server.listen.host + ':' + config.server.listen.port + '/' + portal.id.toLowerCase() + site;
-		console.log(url);
-		console.time('took');
-		request(url, {timeout: 300000}, (error, response, body) => {
-			console.timeEnd('took');
-			if (error) console.log(error);
-			next();
+let sites = [];
+
+routes.forEach(route => {
+	if (route.rootHTML5 || route.rootMenu) {
+		sites.push('/' + route.path);
+	}
+	if (route.children) {
+		route.children.forEach(sub => {
+			sites.push('/' + route.path + '/' + sub.path);
 		});
+	}
+});
+
+let warm_site = (portal, site, lang, cb) => {
+	let url = 'http://' + config.server.listen.host + ':' + config.server.listen.port + '/' + portal.id.toLowerCase() + site + (lang.id !== 'en' ? '?lang=' + lang.id : '');
+	console.time(url);
+	request(url, {timeout: 300000}, (error, response, body) => {
+		console.timeEnd(url);
+		if (error) console.log(error);
+		cb();
+	});
+};
+
+let warm_sites = (cb) => {
+	async.forEachSeries(languages, (lang, nextlang) => {
+		async.forEachSeries(portals, (portal, nextportal) => {
+			async.forEachSeries(sites, (site, nextsite) => {
+				warm_site(portal, site, lang, nextsite);
+			}, nextportal);
+		}, nextlang);
 	}, cb);
 };
 
-
-async.forEachSeries(sites, warm_site, () => {
+warm_sites(() => {
 	console.log('done');
 });
